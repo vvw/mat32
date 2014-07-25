6befc156ed0a8c680f084d88e34afd8444787c48
vvw <siluplsy@gmail.com>
2014-07-25 11:10:18 +0800

    bwlabel funtion useage
    bwlabel funtion useage

---
+++ bwlabel/bwlbl.m
@@ -0,0 +1,16 @@
+﻿% 对连通对象进行标记，分为4 邻接和8 邻接。图像中连在一起的认为是同一个对象的一部分。
+
+% Create a small binary image to use for this example.
+BW = logical ([1     1     1     0     0     0     0     0
+               1     1     1     0     1     1     0     0
+               1     1     1     0     1     1     0     0
+               1     1     1     0     0     0     1     0
+               1     1     1     0     0     0     1     0
+               1     1     1     0     0     0     1     0
+               1     1     1     0     0     1     1     0
+               1     1     1     0     0     0     0     0])
+
+L = bwlabel(BW, 4)  % 4 邻域  % size L = size BW , L 的值为1, 2, 3 ... 分别表示第一个对象，第二个对象，...
+%  get the row and column coordinates of the object labeled "2"
+[r, c] = find(L==2);
+rc = [r c]
